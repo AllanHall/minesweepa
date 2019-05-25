b104393d23fe6af5491de313a4e464a82a8b818a
Allan Hall <bigal76126@gmail.com>
2019-05-25 17:51:36 -0400

    inital api data post, received new game

--- src/App.js
+++ src/App.js
@@ -1,9 +1,31 @@
 import React, { Component } from 'react'
-import HelloWorld from './components/HelloWorld'
+import Header from './components/Header'
 
 class App extends Component {
+  componentDidMount() {
+    fetch('https://minesweeper-api.herokuapp.com/games', {
+      method: 'POST',
+      headers: {
+        'Content-type': 'application/json'
+      },
+      body: JSON.stringify({ difficulty: 0 })
+    })
+      .then(resp => {
+        return resp.json()
+      })
+      .then(game => {
+        console.log(game)
+      })
+  }
   render() {
-    return <HelloWorld />
+    return (
+      <>
+        <Header />
+        <main>
+          <div className="game-box" />
+        </main>
+      </>
+    )
   }
 }
 
---
+++ src/components/Header.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react'
+
+class Header extends Component {
+  render() {
+    return <header className="header">Minesweeper</header>
+  }
+}
+
+export default Header
