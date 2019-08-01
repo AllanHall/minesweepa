84ba7520ef0b071b7e53e436ead41b468af11aee
Allan Hall <bigal76126@gmail.com>
2019-07-31 21:33:44 -0400

    almost implemented phasers, encountering error

--- src/App.js
+++ src/App.js
@@ -3,6 +3,18 @@ import Header from './components/Header'
 
 import sound from './sounds/sound.mp3'
 
+const width = 2600
+const height = 900
+
+const game = new Phaser.Game(width, height, Phaser.AUTO, 'phaser-example', {
+  preload: preload,
+  create: create,
+  update: update
+})
+
+const sprite
+const deadGroup = null
+
 class App extends Component {
   state = {
     game: {},
@@ -13,6 +25,60 @@ class App extends Component {
   }
   componentDidMount() {
     this.resetGame()
+  }
+
+  preload() {
+    game.load.crossOrigin = 'Anonymous'
+    game.load.spritesheet(
+      'ms',
+      'https://jjwallace.github.io/assets/examples/images/boom.png',
+      256,
+      256,
+      64
+    )
+  }
+
+  create() {
+    createSprite(
+      random(0, width),
+      random(0, height),
+      randomRotation(),
+      Math.floor(Math.random() * 30)
+    )
+  }
+
+  createSprite(x, y, r, f) {
+    deadGroup = game.add.group()
+    sprite = game.add.sprite(40, 100, 'ms')
+    sprite.x = x
+    sprite.y = y
+    sprite.angle = r
+    var anim = sprite.animations.add('boom')
+    anim.frame = f
+    anim.play('boom', 60, false)
+    anim.killOnComplete = true
+    anim.onComplete.add(function() {
+      deadGroup.add(this)
+    }, sprite)
+  }
+
+  update() {
+    if (deadGroup != null) {
+      deadGroup.forEach(function(sprite) {
+        console.debug('destroying:', sprite.name)
+        sprite.destroy()
+        console.log('destroyed: ', sprite)
+      })
+    }
+    createSprite(random(0, width), random(0, height), randomRotation(), 0)
+  }
+
+  random(min, max) {
+    return Math.floor(Math.random() * max + min)
+  }
+
+  randomRotation() {
+    return Math.floor(Math.random() * 180 - 180)
   }
 
   checkGame = (row, column) => {
